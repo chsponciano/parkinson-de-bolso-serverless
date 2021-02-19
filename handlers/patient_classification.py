0bc7fd5897462ec73a33708a7dce0a994ee500bb
chsponciano <chs.ponciano@gmail.com>
2021-02-18 21:26:54 -0300

    initial implementation for parkinson's prediction

--- handlers/patient_classification.py
+++ handlers/patient_classification.py
@@ -1,5 +1,6 @@
 import json
 import os
+import uuid
 import boto3
 
 from boto3.dynamodb.conditions import Attr
@@ -9,6 +10,12 @@ from util.decimal_encoder import DecimalEncoder
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['PATIENT_CLASSIFICATION_TABLE'])
 
+class PatientClassificationModel:
+    def __init__(self, data):
+        self.patientid = data['patientid']
+        self.percentage = data['percentage']
+        self.date = data['date']
+        
 def get_all(event, context):
     _patiensClassifications = table.scan(
         FilterExpression=Attr('patientid').eq(event['pathParameters']['patientid'])
@@ -26,4 +33,14 @@ def delete(event, context):
     )
     return {
         'statusCode': 200
+    }
+
+def create(event, context):    
+    _patiensClassifications = PatientClassificationModel(json.loads(event['body'])).__dict__
+    _patiensClassifications['id'] = str(uuid.uuid1())
+    table.put_item(Item=_patiensClassifications)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(_patiensClassifications)
     }
--- handlers/predict.py
+++ handlers/predict.py
@@ -1,23 +1,55 @@
 import json
 import os
+import time
+import uuid
+import base64
+import random
 import boto3
+
+from boto3.dynamodb.conditions import Attr
 
 
 dynamodb = boto3.resource('dynamodb')
-active_classification_table = dynamodb.Table(os.environ['ACTIVE_CLASSIFICATION_TABLE'])
 executation_classification_table = dynamodb.Table(os.environ['EXECUTATION_CLASSIFICATION_TABLE'])
 
-def initialize(event, context):
-    return {
-        'statusCode': 200,
-    }
-
 def evaluator(event, context):
+    _timestamp = str(time.time())
+    _executation = json.loads(event['body'])
+    _executation['id'] = str(uuid.uuid1())
+    _executation['createdAt'] = _timestamp
+    _executation['updatedAt'] = _timestamp
+
+    # start of implementation
+    _executation_image = base64.b64decode(_executation['image']['data'])
+    del _executation['image']
+
+    # temporary random value until model creation
+    _executation['percentage'] = str(random.randint(0, 100))
+
+    executation_classification_table.put_item(Item=_executation)
     return {
         'statusCode': 200,
+        'body': json.dumps(_executation)
     }
 
 def conclude(event, context):
+    _id = event['pathParameters']['patientid']
+    _executations = executation_classification_table.scan(
+        FilterExpression=Attr('patientid').eq(_id)
+    )['Items']
+
+    _list_size = len(_executations)
+    _sum_percentages = 0
+
+    for e in _executations:
+        _sum_percentages += int(e['percentage'])
+        executation_classification_table.delete_item(Key={ 'id': e['id'] })
+
+    _sum_percentages = int(_sum_percentages / _list_size)
+
     return {
         'statusCode': 200,
+        'body': json.dumps({
+            'percentage': str(_sum_percentages)
+        })
     }
