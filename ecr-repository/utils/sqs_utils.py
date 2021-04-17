a7865242945d7809218d13688033ed56d2e917e0
chsponciano <chs.ponciano@gmail.com>
2021-04-17 15:17:29 -0300

    adjustments in the output of the prediction values

--- ecr-repository/utils/sqs_utils.py
+++ ecr-repository/utils/sqs_utils.py
@@ -20,48 +20,40 @@ def SQSProducer(queue_url, message):
         MessageDeduplicationId=_id
     )
 
-class Receive(Entrypoint):
+class Consumer(Entrypoint):
 
-    def __init__(self, queue_url, region="sa-east-1", **kwargs):
-        self.url = queue_url
-        self.region = os.environ.get('AWS_REGION')
-        super(Receive, self).__init__(**kwargs)
-
-    def setup(self):
-        self.client = boto3.client('sqs', region_name=self.region)
+    def __init__(self, queue_url, **kwargs):
+        self._queue_url = queue_url
+        super(Consumer, self).__init__(**kwargs)
 
     def start(self):
-        self.container.spawn_managed_thread(
-            self.run, identifier="Receive.run"
-        )
+        self.container.spawn_managed_thread(self.run, identifier="Consumer.run")
 
     def run(self):
         while True:
-            response = self.client.receive_message(
-                QueueUrl=self.url,
+            _response = SQS_CLIENT.receive_message(
+                QueueUrl=self._queue_url,
                 WaitTimeSeconds=5,
             )
-            messages = response.get('Messages', ())
-            for message in messages:
-                self.handle_message(message)
 
-    def handle_message(self, message):
-        handle_result = partial(self.handle_result, message)
+            for message in _response.get('Messages', ()):
+                self._handle_message(message)
 
-        args = (message['Body'],)
-        kwargs = {}
+    def _handle_message(self, message):
+        _handle_result = partial(self._handle_result, message)
 
         self.container.spawn_worker(
-            self, args, kwargs, handle_result=handle_result
+            self, 
+            (message['Body'],), 
+            {}, 
+            handle_result=_handle_result
         )
 
-    def handle_result(self, message, worker_ctx, result, exc_info):
-        # assumes boto client is thread-safe for this action, because it
-        # happens inside the worker threads
-        self.client.delete_message(
-            QueueUrl=self.url,
+    def _handle_result(self, message, worker_ctx, result, exc_info):
+        SQS_CLIENT.delete_message(
+            QueueUrl=self._queue_url,
             ReceiptHandle=message['ReceiptHandle']
         )
         return result, exc_info
 
-SQSConsumer = Receive.decorator
+SQSConsumer = Consumer.decorator
