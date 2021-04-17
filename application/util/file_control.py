76a13a262a3efd5aa40f8c3c2c8ef49f848f494b
chsponciano <chs.ponciano@gmail.com>
2021-04-16 21:30:01 -0300

    infrastructure adjustments for asynchronous message exchange

--- application/util/file_control.py
+++ application/util/file_control.py
@@ -4,20 +4,20 @@ import uuid
 import boto3
 
 
-_s3_resource = boto3.resource("s3")
-_s3_client = boto3.client('s3')
-_bucket = os.environ['BUCKET_NAME']
+S3_RESOURCE = boto3.resource("s3")
+S3_CLIENT = boto3.client('s3')
+BUCKET_NAME = os.environ['BUCKET_NAME']
 
 def add(file_data, path=''):
     _filename = path + get_uuid_name(file_data['filename'])
-    _object = _s3_resource.Object(_bucket, _filename)
+    _object = S3_RESOURCE.Object(BUCKET_NAME, _filename)
     _object.put(Body=to_byte(file_data['data']))
     _assign_public_reading(_filename)
     return _get_url(_filename)
 
 def delete(filename):
     _filename = filename.split('/')[-1]
-    _s3_resource.Object(_bucket, _filename).delete()
+    S3_RESOURCE.Object(BUCKET_NAME, _filename).delete()
 
 def to_byte(encoded_file):
     return base64.b64decode(encoded_file)
@@ -26,9 +26,9 @@ def get_uuid_name(filename):
     return str(uuid.uuid1()) + os.path.splitext(filename)[-1]
 
 def _assign_public_reading(filename):
-    _object_acl = _s3_resource.ObjectAcl(_bucket, filename)
+    _object_acl = S3_RESOURCE.ObjectAcl(BUCKET_NAME, filename)
     _object_acl.put(ACL='public-read')
 
 def _get_url(filename):
-    _location = _s3_client.get_bucket_location(Bucket=_bucket)['LocationConstraint']
-    return "https://{}.s3-{}.amazonaws.com/{}".format(_bucket, _location, filename)
+    _location = S3_CLIENT.get_bucket_location(Bucket=BUCKET_NAME)['LocationConstraint']
+    return "https://{}.s3-{}.amazonaws.com/{}".format(BUCKET_NAME, _location, filename)
