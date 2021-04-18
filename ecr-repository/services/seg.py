e4626b660a425b21f1a98f9c5f7e5c2741a32585
chsponciano <chs.ponciano@gmail.com>
2021-04-18 19:11:24 -0300

    data output test

---
+++ ecr-repository/services/seg.py
@@ -0,0 +1,38 @@
+import gc
+import cv2
+import argparse
+import skimage.io
+import numpy as np
+import keras.backend as K
+from pixellib.instance import instance_segmentation
+
+
+def _get_silhouette(self, mask, file_path):
+    image = skimage.io.imread(file_path)
+    for i in range(mask.shape[2]):
+        for j in range(image.shape[2]):
+            image[:,:,j] = image[:,:,j] * mask[:,:,i]
+    _, _segmented_img = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)
+    return _segmented_img
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-sm", "--segmentation_model", required=True, help='segmentation model path')
+ap.add_argument("-f", "--file", required=True, help='file path')
+args = vars(ap.parse_args())
+
+instance_segmentation = instance_segmentation()
+instance_segmentation.load_model(args['segmentation_model'])
+target_classes = instance_segmentation.select_target_classes(person=True)
+file_path = args['file']
+
+mask, _ = instance_segmentation.segmentImage(
+    file_path, 
+    segment_target_classes=_target_classes, 
+    extract_segmented_objects=True
+)
+
+mask = mask['masks'].astype(int)
+cv2.imwrite(file_path, self._get_silhouette(mask, file_path))
+K.clear_session()
+del instance_segmentation
+gc.collect()
--- ecr-repository/services/segmentation_service.py
+++ ecr-repository/services/segmentation_service.py
@@ -42,7 +42,7 @@ class SegmentationService:
 
     def run(self, body):
         # initialize the segmentation instance
-        _instance_segmentation = None
+        # _instance_segmentation = None
 
         # converting from string to map
         body = json.loads(body)
@@ -53,25 +53,27 @@ class SegmentationService:
             _file_path = download_image(wait_url)  
             body['local_image'] = _file_path
 
+            os.system('python3 ' + os.path.join(os.getcwd(), 'services/seg.py') + ' -sm ' + self._segmentation_model_path + ' -f ' + _file_path)
+
             # load segmentation model
-            _instance_segmentation = self._load_segmentation_model()
+            # _instance_segmentation = self._load_segmentation_model()
 
             # creates the segmentation target
-            _target_classes = _instance_segmentation.select_target_classes(person=True)
+            # _target_classes = _instance_segmentation.select_target_classes(person=True)
 
             # target person in the image
-            _mask, _ = _instance_segmentation.segmentImage(
-                _file_path, 
-                segment_target_classes=_target_classes, 
-                extract_segmented_objects=True
-            )
+            # _mask, _ = _instance_segmentation.segmentImage(
+            #     _file_path, 
+            #     segment_target_classes=_target_classes, 
+            #     extract_segmented_objects=True
+            # )
 
             # convert targeting values ​​to integer
-            _mask = _mask['masks'].astype(int)
+            # _mask = _mask['masks'].astype(int)
 
             # get silhouette of the person in the image and
             # save the new image in the local folder
-            cv2.imwrite(_file_path, self._get_silhouette(_mask, _file_path))
+            # cv2.imwrite(_file_path, self._get_silhouette(_mask, _file_path))
 
             # saves the segmented image on s3
             body['url_image'] = add_collection_image(_file_path)
@@ -89,9 +91,9 @@ class SegmentationService:
 
         finally:
             # clears the segmentation instance
-            K.clear_session()
-            del _instance_segmentation
-            gc.collect()
+            # K.clear_session()
+            # del _instance_segmentation
+            # gc.collect()
             delete_standby_image(wait_url)
 
         return body
