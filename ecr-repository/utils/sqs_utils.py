3584eb10f78d48d443737e7c62b72fa2535c162d
chsponciano <chs.ponciano@gmail.com>
2021-05-03 20:29:21 -0300

    test

--- ecr-repository/utils/sqs_utils.py
+++ ecr-repository/utils/sqs_utils.py
@@ -13,17 +13,18 @@ class SQSProducer:
         self.setup()
 
     def setup(self):
-        self._sqs_client = boto3.client('sqs', region_name=self._region)
+        self._sqs_client = boto3.client('sqs')
 
     def run(self, message):
-        print('[%s][%s] message posting - Queue: %s' % (time.ctime(time.time()), self._region, self._queue_url))
         _id = uuid.uuid4().hex
-        return self._sqs_client.send_message(
+        _situation = self._sqs_client.send_message(
             QueueUrl=self._queue_url,
             MessageBody=json.dumps(message),
             MessageGroupId=_id,
             MessageDeduplicationId=_id
         )
+        print('[%s][%s] message posting - Situation: %s | Queue: %s' % (time.ctime(time.time()), _id, _situation, self._queue_url))
+
         
 
 class SQSConsume(threading.Thread):
